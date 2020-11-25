25e3a9bf6b99ac08077709f993fce3bbf899ff2e
Kevon1230 <kev-g@wp.pl>
2020-11-25 09:29:37 +0100

    added final project

---
+++ Assets/Scripts/BoardGenerator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class BoardGenerator : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector2 boardBorders;
+    [SerializeField] private float generationoffset = 10f;
+    [SerializeField] private float generationdistance = 100f;
+    [SerializeField] private float destructionDistance = 20f;
+    [SerializeField] private GameObject obstaclePrefab;
+    [SerializeField] private Transform obstaclesParent;
+    private float lastZOffset;
+
+    private void Start()
+    {
+        GenerateObstacles()
+
+    }
+
+    private void GenerateObstacles()
+    {
+        while(lastZOffset < target.position.z + generationdistance)
+        {
+            Instantiate(obstaclePrefab, Vector3.zero, Quaternion.identity, obstaclesParent);
+            lastZOffset += generationoffset;
+        }
+    }
+
+    private void GenerateObstacle()
+    {
+
+    }
+}
